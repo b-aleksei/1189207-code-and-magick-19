2fd3df88c05ccfdb094bdcbb7630182f64af80da
Aleksei Bugaev <103301@mail.ru>
2020-01-25 12:18:00 +0300

    make universal function

--- js/stat.js
+++ js/stat.js
@@ -8,6 +8,7 @@ var SHADOW = 'rgba(0, 0, 0, 0.7)';
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var MAX_BAR = -150;
+var FONT = '16px PT Mono';
 var getRandomPercent = function () {
   return Math.random().toFixed(2) * 100;
 };
@@ -23,30 +24,32 @@ function getMaxElement(arr) {
   return maxElement;
 }
 
-function makeRect(ctx, x, y, color) {
-  ctx.fillStyle = SHADOW;
-  ctx.fillRect(x + GAP, y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+function makeRect(ctx, x, y, width, height, color) {
+  ctx.fillStyle = color || '#000000';
+  ctx.fillRect(x, y, width, height);
+}
+
+function makeText(ctx, text, x, y, font, color) {
+  ctx.fillStyle = color || '#000000';
+  ctx.font = font || FONT;
+  ctx.fillText(text, x, y);
 }
 
 window.renderStatistics = function (ctx, names, time) {
-  makeRect(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-  ctx.fillStyle = '#000000';
-  ctx.font = '16px PT Mono';
+  makeRect(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW);
+  makeRect(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#ffffff');
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + CLOUD_X + GAP * 4, CLOUD_Y + GAP * 2);
-  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_X + GAP * 3, CLOUD_Y + GAP * 4);
+  makeText(ctx, 'Ура вы победили!', CLOUD_X + CLOUD_X + GAP * 4, CLOUD_Y + GAP * 2);
+  makeText(ctx, 'Список результатов:', CLOUD_X + CLOUD_X + GAP * 3, CLOUD_Y + GAP * 4);
 
   var maxTime = getMaxElement(time);
 
   for (var i = 0; i < time.length; i++) {
     var barHeight = MAX_BAR * time[i] / maxTime;
     var x = (CLOUD_X + GAP * 3) + (BAR_WIDTH + BAR_GAP) * i;
-    ctx.fillStyle = 'black';
-    ctx.fillText(names[i], x, CLOUD_HEIGHT - GAP * 2);
-    ctx.fillText(Math.round(time[i]), x, barHeight + CLOUD_HEIGHT - GAP * 4);
-    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(255,' + getRandomPercent() + '%, 50%)';
-    ctx.fillRect(x, CLOUD_HEIGHT - GAP * 2, BAR_WIDTH, barHeight);
+    makeText(ctx, names[i], x, CLOUD_HEIGHT - GAP * 2);
+    makeText(ctx, Math.round(time[i]), x, barHeight + CLOUD_HEIGHT - GAP * 4);
+    var colorBar = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(255,' + getRandomPercent() + '%, 50%)';
+    makeRect(ctx, x, CLOUD_HEIGHT - GAP * 2, BAR_WIDTH, barHeight, colorBar);
   }
 };
