afd45574eb388e24425ce3cd765c1f5dccb8290a
Aleksei Bugaev <103301@mail.ru>
2020-02-14 07:58:32 +0300

    done homework

--- js/backend.js
+++ js/backend.js
@@ -9,7 +9,7 @@
     200: 'OK',
     404: 'Cтраница не найдена, проверьте коректность адреса',
     500: 'Сервер временно не доступен, скоро все заработает',
-    unknown: 'Неизвестная ошибка, попробуйте позднее'
+    unknown: 'Неизвестная ошибка, попробуйте позднее',
   };
 
   var errorHandler = function (errorMessage) {
@@ -22,53 +22,55 @@
       node.remove();
     }, 3000);
   };
-/* eslint-disable */
-  var onHandlerLoad = function (success, error) {
-    switch (this.status) {
-      case 200 : success(this.response);
-      break;
-      case 404 : error(this.status + ' ' + errorType[404]);
-      break;
-      case 500 : error(this.status + ' ' + errorType[500]);
-      break;
-      default : error(this.status + ' ' + errorType.unknown);
-    }
-  };
 
-  var onHandlerTimeOut = function (error) {
-    error('Запрос не успел выполниться за ' + this.timeout + 'мс');
-  };
-  var onHandlerError = function (error) {
-    error('Произошла ошибка соединения. Проверьте подключение к интернет');
-  };
-  /* eslint-enable */
-  var load = function (success, error) {
+  var startRequest = function (success, error) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.addEventListener('load', function () {
+      switch (xhr.status) {
+        case 200 :
+          success(xhr.response);
+          break;
+        case 404 :
+          error(xhr.status + ' ' + errorType[404]);
+          break;
+        case 500 :
+          error(xhr.status + ' ' + errorType[500]);
+          break;
+        default :
+          error(xhr.status + ' ' + errorType.unknown);
+      }
+    }, {once: true});
+
+    xhr.addEventListener('error', function () {
+      error('Произошла ошибка соединения. Проверьте подключение к интернет');
+    }, {once: true});
+
+    xhr.addEventListener('timeout', function () {
+      error('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    }, {once: true});
+
+    return xhr;
+  };
+
+  var load = function (success, error) {
+    var xhr = startRequest(success, error);
     xhr.open('GET', URL_LOAD);
     xhr.send();
-
-    xhr.addEventListener('load', onHandlerLoad.bind(xhr, success, error));
-    xhr.addEventListener('error', onHandlerError.bind(xhr, error));
-    xhr.addEventListener('timeout', onHandlerTimeOut.bind(xhr, error));
   };
 
   var save = function (data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
+    var xhr = startRequest(success, error);
     xhr.open('POST', URL_SAVE);
     xhr.send(data);
-
-    xhr.addEventListener('load', onHandlerLoad.bind(xhr, success, error));
-    xhr.addEventListener('error', onHandlerError.bind(xhr, error));
-    xhr.addEventListener('timeout', onHandlerTimeOut.bind(xhr, error));
   };
 
   window.backend = {
     load: load,
     save: save,
-    errorHandler: errorHandler
+    errorHandler: errorHandler,
   };
 
 })();
